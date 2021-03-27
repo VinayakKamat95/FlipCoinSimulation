3f2709a8484f3466a6cf4276506508507b00ee8c
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-03-27 16:56:35 +0530

    Flipping a Coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,11 @@
-#Welcome to Flip Coin Simulation Program
+#!/bin/bash -x
+
+bin=$((RANDOM%2))
+
+if [ $bin -eq 1 ]
+then
+        coin="heads"
+else
+        coin="tails"
+fi
+echo "winner : " $coin
