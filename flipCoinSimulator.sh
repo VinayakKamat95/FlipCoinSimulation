a53a92655e10925d441ee522a4067ff8ae73cd46
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-03-27 17:04:51 +0530

    Added : Loop through Multiple times of flipping a coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,31 @@
 #!/bin/bash -x
 
-bin=$((RANDOM%2))
+#CONSTANT
+IS_HEADS=1
 
-if [ $bin -eq 1 ]
-then
-        coin="heads"
-else
-        coin="tails"
-fi
-echo "winner : " $coin
+#variables
+headCount=0
+tailCount=0
+count=0
+
+while [ $count -ne 10  ]
+do
+
+	bin=$((RANDOM%2))
+
+	if [ $bin -eq $IS_HEADS ]
+	then
+		coin="heads"
+		headCount=$(($headCount+1))
+	else
+		coin="tails"
+		tailCount=$(($tailCount+1))
+	fi
+
+((count++))
+
+done
+
+echo "Heads wins : $headCount times"
+
+echo "Tails wins : $tailCount times"
