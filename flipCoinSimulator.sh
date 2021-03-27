ed631c0465dcc7ed9e31b9d357e02af4d08cb2ba
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-03-27 18:49:10 +0530

    Flipping coin till one wins 21 times or tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,31 +1,43 @@
 #!/bin/bash -x
+IS_HEAD=1
 
-#CONSTANT
-IS_HEADS=1
+headCount=0;
+tailCount=0;
 
-#variables
-headCount=0
-tailCount=0
-count=0
-
-while [ $count -ne 10  ]
+while [ 1 ]
 do
+   bin=$((RANDOM%2));
 
-	bin=$((RANDOM%2))
+   if [ $bin -eq $IS_HEAD ];
+   then
+      coin="heads";
+      headCount=$(($headCount+1));
+   else
+      coin="tails";
+      tailCount=$(($tailCount+1));
+   fi
 
-	if [ $bin -eq $IS_HEADS ]
-	then
-		coin="heads"
-		headCount=$(($headCount+1))
-	else
-		coin="tails"
-		tailCount=$(($tailCount+1))
-	fi
-
-((count++))
+   if [[ $headCount -eq 21 || $tailCount -eq 21 ]];
+   then
+      break;
+   fi
 
 done
 
-echo "Heads wins : $headCount times"
+echo "head Count:"$headCount;
+echo "tail count:"$tailCount;
 
-echo "Tails wins : $tailCount times"
+if [ $headCount -gt $tailCount ];
+then
+   echo "Head wins : by $(($headCount-$tailCount))";
+
+elif [ $headCount -lt $tailCount ];
+then
+   echo "Tails wins : by $(($tailCount-$headCount))";
+
+else
+
+   echo "Tie";
+
+fi
+
