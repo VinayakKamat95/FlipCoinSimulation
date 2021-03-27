f36accdbdc54319cec2f948677da99240b188191
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-03-27 19:03:49 +0530

    Added : Logic for Tie Game

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,43 +1,63 @@
 #!/bin/bash -x
+
 IS_HEAD=1
 
-headCount=0;
-tailCount=0;
+headCount=0
+tailCount=0
 
-while [ 1 ]
-do
-   bin=$((RANDOM%2));
+function tieGame(){
+   while [[ $(($headCount-$tailCount)) -lt 2 && $(($tailCount-$headCount)) -lt 2 ]]
+   do
+   bin=$((RANDOM%2))
 
-   if [ $bin -eq $IS_HEAD ];
+   if [ $bin -eq $IS_HEAD]
    then
-      coin="heads";
-      headCount=$(($headCount+1));
+      coin="heads"
+      headCount=$(($headCount+1))
    else
-      coin="tails";
-      tailCount=$(($tailCount+1));
+      coin="tails"
+      tailCount=$(($tailCount+1))
    fi
+   done
+   echo $(GameResult)
+}
 
-   if [[ $headCount -eq 21 || $tailCount -eq 21 ]];
+
+function GameResult(){
+
+if [ $headCount -gt $tailCount ]
+then
+   echo "Head wins : by $(($headCount-$tailCount))"
+
+elif [ $headCount -lt $tailCount ]
+then
+   echo "Tails wins : by $(($tailCount-$headCount))"
+
+else
+   echo "Tie Game"
+   echo $( tieGame )
+fi
+
+}
+
+
+while [[ $headCount -lt 21 && $tailCount -lt 21 ]]
+do
+   bin=$((RANDOM%2))
+
+   if [ $bin -eq 1 ]
    then
-      break;
+      coin="heads"
+      headCount=$(($headCount+1))
+   else
+     coin="tails"
+      tailCount=$(($tailCount+1))
    fi
 
 done
 
-echo "head Count:"$headCount;
-echo "tail count:"$tailCount;
+echo "head Count:"$headCount
+echo "tail count:"$tailCount
 
-if [ $headCount -gt $tailCount ];
-then
-   echo "Head wins : by $(($headCount-$tailCount))";
-
-elif [ $headCount -lt $tailCount ];
-then
-   echo "Tails wins : by $(($tailCount-$headCount))";
-
-else
-
-   echo "Tie";
-
-fi
+echo $(GameResult)
 
